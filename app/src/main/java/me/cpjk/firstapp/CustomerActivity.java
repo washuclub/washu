8daea5c3d85c5d404e45ef7cf1b3426e13455213
Chris Kelly <cpjkelly@gmail.com>
2016-03-27 16:51:11 -0700

    Initial commit

---
+++ app/src/main/java/me/cpjk/firstapp/CustomerActivity.java
@@ -0,0 +1,22 @@
+package me.cpjk.firstapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
